5b36914b8eed511aaa921fcba7758f93df1df8f5
Lucas Gabriel Becker <lucas.gabriel.becker.08@gmail.com>
2024-09-11 08:14:00 -0300

    Commit Cinco

--- back/index.js
+++ back/index.js
@@ -9,19 +9,33 @@ app.use(cors())
 app.post("/cadastro", function(req, res){
     console.log('cadastro', req.body)
     db.serialize(function(){
-        db.run("insert into users(email, pass) values(?, ?)", [req.body.email, req.body.pass])
+        db.run("insert into users(name, email, pass) values(?, ?, ?)", [req.body.name, req.body.email, req.body.pass])
     })
     db.close()
     res.send('dale')
 })
 
-app.get("/login/:id", function(req, res){
-    const id = req.params.id
-    db.serialize(function(){
-        db.run("select")
-    })
+/*db.serialize(function(){
+    db.all("SELECT id FROM users WHERE email = ? and pass = ?", [email, pass], function(err, rows) {
+        if(err) return;
+        rows.forEach(function (row) {
+            console.log(row.id);
+        })
+	});	
 })
-
+db.close();*/
+/*
+app.get("/login", function(req, res){
+    db.serialize(function(){
+        db.all("SELECT * FROM users", function(err, rows) {
+            rows.forEach(function (row) {
+                console.log(row.email, row.pass);
+            })
+        });	
+    })
+    db.close();
+})
+*/
 /*app.get("/games/:id", function(req, res){
     const id = req.params.id
 })*/
