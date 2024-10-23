40ddea5283c3dce57cb95cffda3ba7fd972fc83a
CaikeWarmeling <128308098+CaikeWarmeling@users.noreply.github.com>
2024-10-23 08:55:44 -0300

    cadastro empresas

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express"
+import bodyparser from "body-parser"
+import cors from "cors"
+import db from "./db.js"
+
+const app = express()
+app.use(bodyparser.json())
+app.use(cors())
+app.post("/cadastro", function(req, res){
+    console.log('cadastro', req.body)
+    db.serialize(function(){
+        db.run("insert into users(name, email, pass) values(?, ?, ?)", [req.body.name, req.body.email, req.body.pass])
+    })
+    db.close()
+    res.send('dale')
+})
+
+app.post("/login", function(req, res){
+    db.serialize(function(){
+        db.all("SELECT id FROM users WHERE email = ? and pass = ?", [req.body.email, req.body.pass], function(err, rows) {
+            if(err) return res.sendStatus(401);
+            if(rows && rows[0] && rows[0].id) return res.sendStatus(200);
+            res.sendStatus(401);
+        });	
+    })
+    //db.close();
+})
+
+app.post("/cadastroEmpresas", function(req, res){
+    console.log('cadastro', req.body)
+    db.serialize(function(){
+        db.run("insert into users(name, email, cnpj) values(?, ?, ?)", [req.body.name, req.body.email, req.body.cnpj])
+    })
+    db.close()
+    res.send('dale')
+})
+
+/*app.get("/games/:id", function(req, res){
+    const id = req.params.id
+})*/
+
+app.listen(3000,function(){console.log('Servidor iniciado!')})
